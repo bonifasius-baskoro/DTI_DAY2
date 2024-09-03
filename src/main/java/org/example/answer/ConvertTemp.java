736e22f4dc73a395db5b3fdf7e535e5c4bd9485c
bonifasiusbask <71079162+bonifasius-baskoro@users.noreply.github.com>
2024-09-03 15:29:20 +0700

    Feat/answer (#1)
    * feat(answer): add ConvertTemp class for temperature conversion
    
    * feat(answer): update ConvertTemp class
    
    * feat(answer): update ConvertTemp class v2
    
    ---------
    
    Co-authored-by: Jerry Baskoro <bjbbeat@gmail.com>

---
+++ src/main/java/org/example/answer/ConvertTemp.java
@@ -0,0 +1,24 @@
+package org.example.answer;
+
+public class ConvertTemp {
+    double fahrenheit;
+    double celcius;
+
+    public ConvertTemp(double fahrenheit) {
+        this.fahrenheit = fahrenheit;
+    }
+
+    public double getFahrenheit() {
+        return fahrenheit;
+    }
+
+    public void setFahrenheit(double fahrenheit) {
+        this.fahrenheit = fahrenheit;
+    }
+
+    public double getCelcius() {
+        this.celcius = (this.fahrenheit-32) *5/9;
+        return celcius;
+    }
+
+}
