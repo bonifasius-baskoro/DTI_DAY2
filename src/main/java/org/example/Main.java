40f770cc49f06e69c495a575444e19afc61eb448
Jerry Baskoro <bjbbeat@gmail.com>
2024-09-03 16:08:19 +0700

    Resolve merge conflict 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,22 @@ import org.example.answer.CentimeterKilometer;
 
 import org.example.answer.ConvertTemp;
 
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        ConvertTemp test1 = new ConvertTemp(320.4);
+        // Press Opt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
 
+        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
 
-        System.out.println(test1.getCelcius());
+            // Press Ctrl+D to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Cmd+F8.
+            System.out.println("i = " + i);
+        }
 
         CentimeterKilometer test2 = new CentimeterKilometer(3403402);
 
