c20b6a9c966b7a8268b04a67f7a5493d78c2136b
Jerry Baskoro <bjbbeat@gmail.com>
2024-09-03 16:41:58 +0700

    feat(answer) : add Palindrome and SearchString

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import org.example.answer.CentimeterKilometer;
-import org.example.answer.ConvertTemp;
-import org.example.answer.OddEven;
-import org.example.answer.SearchString;
+import org.example.answer.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -12,18 +9,22 @@ public class Main {
         ConvertTemp test1 = new ConvertTemp(320.4);
 
 
-        System.out.println(test1.getCelcius());
+        System.out.println("The celcius is :"+test1.getCelcius());
 
         CentimeterKilometer test2 = new CentimeterKilometer(3403402);
 
-        System.out.println(test2.getKilometer());
+        System.out.println("The kilomter is " + test2.getKilometer());
 
         OddEven test3 = new OddEven(5);
-        System.out.println(test3.isOdd());
+        System.out.println("Is it odd number:" + test3.isOdd());
 
         SearchString test4= new SearchString("Ayam Kokok");
 
-        System.out.println(test4.removeString("ko"));
+        System.out.println("What Is the string after we remove:" + test4.removeString("ko"));
+
+        Palindrome test5= new Palindrome("madam");
+
+        System.out.println("Is the word is palindrome : "+ test5.isPalindrome());
 
     }
 }
